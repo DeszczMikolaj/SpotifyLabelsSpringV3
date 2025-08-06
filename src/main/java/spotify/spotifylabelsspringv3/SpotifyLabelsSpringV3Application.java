bb96365a8d14b7b20b9742e1c093f45db0f04fe9
mikolaj.deszcz <mikolaj.deszcz@du.co>
2025-08-06 22:45:21 +0200

    initial commit

---
+++ src/main/java/spotify/spotifylabelsspringv3/SpotifyLabelsSpringV3Application.java
@@ -0,0 +1,13 @@
+package spotify.spotifylabelsspringv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyLabelsSpringV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyLabelsSpringV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/spotify/spotifylabelsspringv3/SpotifyLabelsSpringV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package spotify.spotifylabelsspringv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyLabelsSpringV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
